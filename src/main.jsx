ceac147dd020c2f7d1a81f785aa76a4fe3206b1f
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-08-30 19:50:19 -0300

    add pages and routes

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import Register from './pages/Register';
+import Login from './pages/Login';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  </BrowserRouter>
 )
